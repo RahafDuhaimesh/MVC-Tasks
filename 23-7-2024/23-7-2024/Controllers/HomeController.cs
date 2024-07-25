ca7699df070950155ecfddcb5c8b975a45546da9
RahafDuhaimesh <rahafduhaimesh211@gmail.com>
2024-07-25 18:04:19 +0300

    Task 2 - Pass Data from V to C and vice versa

---
+++ 23-7-2024/23-7-2024/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+
+namespace _23_7_2024.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult ContactForm(FormCollection form)
+        {
+            ViewBag.Name = form["name"];
+            ViewBag.Age = form["age"];
+            ViewBag.Gender = form["gender"];
+            ViewBag.FeedbackType = form["FeedbackType"];
+            ViewBag.Feedback = form["Feedback"];
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult ShowFeedbacks(FormCollection form)
+        {
+            ViewBag.Name = form["name"];
+            ViewBag.Age = form["age"];
+            ViewBag.Gender = form["gender"];
+            ViewBag.FeedbackType = form["FeedbackType"];
+            ViewBag.Feedback = form["Feedback"];
+            return View();
+        }
+        [ActionName("ShowFeedbacks")]
+        public ActionResult ShowFeedbacks()
+        {
+            return View();
+        }
+    }
+}
