94c31a4bc166bfb487f508c7b000968b410b35af
RahafDuhaimesh <rahafduhaimesh211@gmail.com>
2024-08-13 22:12:18 +0300

    Revision Task - Store data in session and list

---
+++ 13-8-2024/13-8-2024/Controllers/ShopController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace _13_8_2024.Controllers
+{
+    public class ShopController : Controller
+    {
+        public ActionResult Index()
+        {
+            var products = Session["Products"] as List<string[]> ?? new List<string[]>();
+            return View(products);
+        }
+
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddProduct(string name, string price, string imgURL)
+        {
+            var newItem = new string[] { name, price, imgURL };
+            var products = Session["Products"] as List<string[]> ?? new List<string[]>();
+            products.Add(newItem);
+            Session["Products"] = products;
+            return RedirectToAction("Index");
+        }
+    }
+}
